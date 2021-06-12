6256c9b4f4b8f5b2512247b58f562f292f13af81
Andrey <forestx24@gmail.com>
2021-06-12 14:27:50 +0200

    bettering navigation

--- assets/js/acordion.js
+++ assets/js/acordion.js
@@ -3,54 +3,45 @@ const LinkLabel = document.querySelectorAll('.label__link')
 const header = document.querySelector('.page__header')
 let headerHeigth = header.scrollHeight
 
-const coverSection = document.querySelector('.cover')
 const coverLabel = document.querySelector('.cover__label')
-let coverHeigth = coverSection.scrollHeight
-let coverLabelHeigth = coverLabel.scrollHeight
-let coverHeigthNecessary = coverHeigth - coverLabelHeigth
 
-const aboutSection = document.querySelector('.about')
 const aboutLabel = document.querySelector('.about__label')
-let aboutHeigth = aboutSection.scrollHeight
-let aboutLabelHeigth = aboutLabel.scrollHeight
-let aboutHeigthNecessary = aboutHeigth - aboutLabelHeigth
 
-const pricingSection = document.querySelector('.pricing')
 const pricingLabel = document.querySelector('.pricing__label')
-let pricingrHeigth = pricingSection.scrollHeight
-let pricingLabelHeigth = aboutLabel.scrollHeight
-let pricingHeigthNecessary = pricingrHeigth - pricingLabelHeigth
 
-const contactSection = document.querySelector('.contact')
 const contactLabel = document.querySelector('.contact__label')
-let contactHeigth = contactSection.scrollHeight
-let contactLabelHeigth = aboutLabel.scrollHeight
-let contactHeigthNecessary = contactHeigth - contactLabelHeigth
+
 
 
 window.onscroll = () => {
+
+    let coverContainer = document.querySelector('.cover__container').scrollHeight
+    let aboutContainer = document.querySelector('.about__container').scrollHeight
+    let pricingContainer = document.querySelector('.pricing__container').scrollHeight
+    let contactContainer = document.querySelector('.contact__container').scrollHeight
+
     let scrollTop = window.scrollY
 
-    if (scrollTop >= coverHeigthNecessary) {
+    if (scrollTop >= coverContainer) {
         coverLabel.classList.add('fixed')
     } else {
         coverLabel.classList.remove('fixed')
     };
 
-    if (scrollTop >= (coverHeigthNecessary + aboutHeigthNecessary)) {
+    if (scrollTop >= (coverContainer + aboutContainer)) {
         aboutLabel.classList.add('fixed')
     } else {
         aboutLabel.classList.remove('fixed')
     };
 
-    if (scrollTop >= (coverHeigthNecessary + aboutHeigthNecessary + pricingHeigthNecessary)) {
+    if (scrollTop >= (coverContainer + aboutContainer + pricingContainer)) {
         pricingLabel.classList.add('fixed')
     } else {
         pricingLabel.classList.remove('fixed')
     };
 
 
-    if (scrollTop >= (coverHeigthNecessary + aboutHeigthNecessary + pricingHeigthNecessary + contactHeigthNecessary)) {
+    if (scrollTop >= (coverContainer + aboutContainer + pricingContainer + contactContainer)) {
         contactLabel.classList.add('fixed')
     } else {
         contactLabel.classList.remove('fixed')
@@ -61,31 +52,34 @@ window.onscroll = () => {
 
 
 const findSection = (e) => {
+
+    let coverContainer = document.querySelector('.cover__container').scrollHeight
+    let aboutContainer = document.querySelector('.about__container').scrollHeight
+    let pricingContainer = document.querySelector('.pricing__container').scrollHeight
+
     let x = e.toElement
-    console.log(x);
 
     if (x.className == 'cover__label label__link fixed') {
         window.scrollTo({
             top: 0,
             behavior: 'smooth',
-            speed: 5000,
         });
     }
     if (x.className == 'about__label label__link fixed') {
         window.scrollTo({
-            top: coverHeigthNecessary,
+            top: coverContainer,
             behavior: 'smooth',
         });
     }
     if (x.className == 'pricing__label label__link fixed') {
         window.scrollTo({
-            top: coverHeigthNecessary + aboutHeigthNecessary,
+            top: coverContainer + aboutContainer,
             behavior: 'smooth',
         });
     }
     if (x.className == 'contact__label label__link fixed') {
         window.scrollTo({
-            top: coverHeigthNecessary + aboutHeigthNecessary + pricingHeigthNecessary,
+            top: coverContainer + aboutContainer + pricingContainer + 5,
             behavior: 'smooth',
         });
     }
@@ -97,3 +91,56 @@ const findSection = (e) => {
 LinkLabel.forEach(el => {
     el.addEventListener('click', findSection)
 })
+
+
+
+//  LAZY SCROOL 
+
+
+
+const menuLinks = document.querySelectorAll('.menu__link');
+
+
+const onMenuLinkClick = (e) => {
+
+    let coverContainer = document.querySelector('.cover__container').scrollHeight
+    let aboutContainer = document.querySelector('.about__container').scrollHeight
+    let pricingContainer = document.querySelector('.pricing__container').scrollHeight
+
+
+    let findLink = e.toElement
+
+    if (findLink.dataset.goto == '.cover') {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+        e.preventDefault();
+    }
+    if (findLink.dataset.goto == '.about') {
+        window.scrollTo({
+            top: coverContainer,
+            behavior: 'smooth',
+        });
+        e.preventDefault();
+    }
+    if (findLink.dataset.goto == '.pricing') {
+        window.scrollTo({
+            top: coverContainer + aboutContainer,
+            behavior: 'smooth',
+        });
+        e.preventDefault();
+    }
+    if (findLink.dataset.goto == '.contact') {
+        window.scrollTo({
+            top: coverContainer + aboutContainer + pricingContainer + 5,
+            behavior: 'smooth',
+        });
+        e.preventDefault();
+    }
+}
+
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,26 +13,26 @@ if (iconMenu) {
 
 
 //  LAZY SCROOL 
+// const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+// if (menuLinks.length > 0) {
+//     menuLinks.forEach(menuLink => {
+//         menuLink.addEventListener('click', onMenuLinkClick);
+//     });
 
-if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener('click', onMenuLinkClick);
-    });
+//     function onMenuLinkClick(e) {
+//         const menuLink = e.target;
+//         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+//             const gotoBlock = document.querySelector(menuLink.dataset.goto);
+//             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+//             console.log(gotoBlock.getBoundingClientRect());
 
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+//             window.scrollTo({
+//                 top: gotoBlockValue,
+//                 behavior: "smooth"
+//             });
+//             e.preventDefault();
 
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-
-        }
-    }
-};
+//         }
+//     }
+// };
